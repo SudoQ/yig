3b112f98e0a4223b00364731083fd5c6be904a4b
Simon Johansson <simj91@gmail.com>
2014-10-09 17:07:41 +0200

    Added support for custom json config

--- main.py
+++ main.py
@@ -2,19 +2,24 @@
 
 #imports
 import pystache as ps
+import sys
 import json
 from views import Invoice
 
 def main():
-	# Load JSON config
-	# Create Invoice with JSON dictionary
-	f = open("invoice.json", "r")
+	configFile = "invoice.json"
+	htmlFile = "invoice.html"
+	if len(sys.argv) == 2 :
+		configFile = sys.argv[1]
+		htmlFile = configFile[:-4] + "html"
+
+	f = open(configFile, "r")
 	file_content = f.read()
 	content = json.loads(file_content)
 	invoice = Invoice(content)
 	renderer = ps.Renderer(file_encoding="utf-8", string_encoding="utf-8")
 	html = renderer.render(invoice)
-	fout = open("invoice.html", "w")
+	fout = open(htmlFile, "w")
 	fout.write(html.encode("UTF-8"))
 
 if __name__ == "__main__":
